b3a3cd34f84f499476dd5a74e62bd389d56636c1
Aario <AarioAi@gmail.com>
2025-04-22 18:42:24 +0800

    feat:typez.Svc

--- typez/svc.go
+++ typez/svc.go
@@ -7,13 +7,16 @@ import (
 
 type Svc atype.Uint24
 
+// Valid 最大值：36^4-1 = 1679615  --> 采用base36编码，4位占位符 1679615
 func (s Svc) Valid() bool {
-	return s > 0
+	return s > 0 && s < 1679615
 }
 
 func (s Svc) String() string {
 	return types.FormatUint(uint64(s))
 }
+
+func (s Svc) Uint32() uint32 { return uint32(s) }
 
 func (s Svc) Or(defaultSvc Svc) Svc {
 	if s.Valid() {
